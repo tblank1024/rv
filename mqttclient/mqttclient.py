4815a59f8b94ec89379f63149aa4d0610acbe1e0
tblank <tblank@ieee.org>
2023-01-27 15:37:14 -0800

    deleted unused files and minor fixes to client

--- mqttclient/mqttclient.py
+++ mqttclient/mqttclient.py
@@ -1,39 +1,86 @@
 #mqtt client to read values of interest from broker
 
+import os
+import time
+import json
 import paho.mqtt.client as mqtt
-import ruamel.yaml as yaml
 from pprint import pprint
 
-with open("subscriptions.yml", "r") as file:
-    data = yaml.load(file, Loader=yaml.Loader)
-    pprint(data)
-    
 
-
-
+TOPIC_PREFIX = 'RVC'
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    global data
+    global TargetTopics, TOPIC_PREFIX
+
     print("Connected with result code "+str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     #client.subscribe("$SYS/#")
-    for val in data['subscriptions']:
-        print("Subscribing to: ",val[0]["dsn_name"], val[0]["qos"])
-        client.subscribe(val[0]["dsn_name"], int(val[0]["qos"]))
+    for name in TargetTopics:
+        #print('Subscribing to: ', key)
+        client.subscribe(name,0)
     print('Running...')
 
-# The callback for when a PUBLISH message is received from the server.
+# The callback for when a PUBLISH message is received from the MQTT server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    global TargetTopics, TopicData, msg_counter
+    #print(msg.topic+ " " + str(msg.payload))
+    msg_dict = json.loads(msg.payload.decode('utf-8'))
+
+    for item in TargetTopics[msg.topic]:
+        if item == 'instance':
+            break
+        #print(item,'= ', msg_dict[item])
+        TopicData[msg.topic + '/' + item] = msg_dict[item]
+
+    msg_counter += 1
+    if msg_counter % 20 == 0:
+        #This is a poor way to provide a UI but tkinter isn't working
+        os.system('clear')
+        print('******************************************************')
+        pprint(TopicData)
+    #time.sleep(.5)
+    
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-#client.connect("mqtt.eclipseprojects.io", 1883, 60)
-client.connect("localhost", 1883, 60)
+try:
+    client.connect("localhost", 1883, 60)
+except:
+    print("Can't connect to MQTT Broker -- exiting")
+    exit()
+
+try:    
+    with open("dgn_variables.json","r") as newfile: 
+        try:
+            data = json.load(newfile)
+        except: 
+            print('Json file format error --- exiting')
+            exit()
+except:
+    print('dgn_variables.json file not found -- exiting')
+    exit()
+
+msg_counter = 0
+TargetTopics = {}
+TopicData = {}
+for item in data:
+    if "instance" in data[item]:
+        topic = TOPIC_PREFIX + '/' + item + '/' + str(data[item]["instance"])
+    else:   
+        topic = TOPIC_PREFIX + '/' + item
+    for entryvar in data[item]:
+        tmp = data[item][entryvar]
+        if  isinstance(tmp, str) and tmp.startswith("Variable"):
+            if topic not in TargetTopics:
+                TargetTopics[topic] = {}
+            TargetTopics[topic][entryvar] = tmp
+            TopicData[topic + '/' + entryvar] = ""
+pprint(TargetTopics)
+pprint(TopicData)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
