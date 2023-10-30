7061e8cecd51ecd6f6fb0d2d79adb0e967be9d72
Tom Blank <tblank@ieee.org>
2023-10-30 15:51:09 -0700

    Improved readability

--- ve.direct/ve-direct.py
+++ ve.direct/ve-direct.py
@@ -7,6 +7,98 @@ import threading
 serial_port = '/dev/ttyAMA0'
 baud_rate = 19200
 
+def charger_status(status):
+    match status:
+        case '0':
+            outstr = "Off"
+        case '1':
+            outstr = "Low power (MPPT not used)"
+        case '2':
+            outstr = "Fault"
+        case '3':
+            outstr = "Bulk"
+        case '4':
+            outstr = "Absorption"
+        case '5':
+            outstr = "Float"
+        case '6':
+            outstr = "Storage (MPPT not used)"
+        case '7':
+            outstr = "Equalize (manual)"
+        case '9':
+            outstr = "Inverting (MPPT not used)"
+        case '11':
+            outstr = "Power supply (MPPT not used)"
+        case '245':
+            outstr = "Starting-up"
+        case '246':
+            outstr = "Repeated absorption (MPPT not used)"
+        case '247':
+            outstr = "Auto equalize"
+        case '248':
+            outstr = "BatterySafe (MPPT not used)"
+        case '252':
+            outstr = "External control"
+        case _:
+            outstr = "Unknown"
+    return outstr
+
+def mppt_status(status):
+    match status:
+        case '0':
+            return("Off")
+        case '1':
+            return("Voltage or current limited")
+        case '2':
+            return("MPPT active")
+        case _:
+            return("Unknown")
+    
+
+def error_status(status):
+    match status:
+        case '0':
+            outstr = "No error"
+        case '2':
+            outstr = "Battery voltage too high"
+        case '17':
+            outstr = "Charger temperature too high"
+        case '18':
+            outstr = "Charger over current"
+        case '19':
+            outstr = "Charger current reversed"
+        case '20':
+            outstr = "Bulk time limit exceeded"
+        case '21':
+            outstr = "Current sensor issue (sensor bias/sensor broken)"
+        case '26':
+            outstr = "Terminals overheated"
+        case '28':
+            outstr = "Converter issue (dual converter models only)"
+        case '33':
+            outstr = "Input voltage too high (solar panel)"
+        case '34':
+            outstr = "Input current too high (solar panel)"
+        case '38':
+            outstr = "Input shutdown (due to excessive battery voltage)"
+        case '39':
+            outstr = "Input shutdown (due to current flow during off mode)"
+        case '65':
+            outstr = "Lost communication with one of devices"
+        case '66':
+            outstr = "Synchronised charging device configuration issue"
+        case '67':
+            outstr = "BMS connection lost"
+        case '116':
+            outstr = "Factory calibration data lost"
+        case '117':
+            outstr = "Invalid/incompatible firmware"
+        case '119':
+            outstr = "User settings invalid"
+        case _:
+            outstr = "Unknown"
+    return outstr
+
 def decode_ve_direct_message(data):
     # VE.Direct message decoding logic
     lst = str(data).split('\\t')
@@ -26,91 +118,13 @@ def decode_ve_direct_message(data):
         case 'PPV':
             print(f"Panel Watts: {lst[1]}")
         case 'CS':
-            match lst[1]:
-                case '0':
-                    outstr = "Off"
-                case '1':
-                    outstr = "Low power (MPPT not used)"
-                case '2':
-                    outstr = "Fault"
-                case '3':
-                    outstr = "Bulk"
-                case '4':
-                    outstr = "Absorption"
-                case '5':
-                    outstr = "Float"
-                case '6':
-                    outstr = "Storage (MPPT not used)"
-                case '7':
-                    outstr = "Equalize (manual)"
-                case '9':
-                    outstr = "Inverting (MPPT not used)"
-                case '11':
-                    outstr = "Power supply (MPPT not used)"
-                case '245':
-                    outstr = "Starting-up"
-                case '246':
-                    outstr = "Repeated absorption (MPPT not used)"
-                case '247':
-                    outstr = "Auto equalize"
-                case '248':
-                    outstr = "BatterySafe (MPPT not used)"
-                case '252':
-                    outstr = "External control"
-                case _:
-                    outstr = "Unknown"
+            outstr = charger_status(lst[1])
             print(f"Charger state: {outstr}")
         case 'MPPT':
-            match lst[1]:
-                case '0':
-                    outstr = "Off"
-                case '1':
-                    outstr = "Voltage or current limited"
-                case '2':
-                    outstr = "MPPT active"
+            outstr = mppt_status(lst[1])
             print(f"MPPT: {outstr}")
         case 'ERR':
-            match lst[1]:
-                case '0':
-                    outstr = "No error"
-                case '2':
-                    outstr = "Battery voltage too high"
-                case '17':
-                    outstr = "Charger temperature too high"
-                case '18':
-                    outstr = "Charger over current"
-                case '19':
-                    outstr = "Charger current reversed"
-                case '20':
-                    outstr = "Bulk time limit exceeded"
-                case '21':
-                    outstr = "Current sensor issue (sensor bias/sensor broken)"
-                case '26':
-                    outstr = "Terminals overheated"
-                case '28':
-                    outstr = "Converter issue (dual converter models only)"
-                case '33':
-                    outstr = "Input voltage too high (solar panel)"
-                case '34':
-                    outstr = "Input current too high (solar panel)"
-                case '38':
-                    outstr = "Input shutdown (due to excessive battery voltage)"
-                case '39':
-                    outstr = "Input shutdown (due to current flow during off mode)"
-                case '65':
-                    outstr = "Lost communication with one of devices"
-                case '66':
-                    outstr = "Synchronised charging device configuration issue"
-                case '67':
-                    outstr = "BMS connection lost"
-                case '116':
-                    outstr = "Factory calibration data lost"
-                case '117':
-                    outstr = "Invalid/incompatible firmware"
-                case '119':
-                    outstr = "User settings invalid"
-                case _:
-                    outstr = "Unknown"
+            outstr = error_status(lst[1])
             print(f"Error: {outstr}")
         case _:  # Default case
             #print(f"Unknown: {lst[0], lst[1]}")
